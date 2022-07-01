195b2b144378202296519166de0773d2f4caeb7e
hmrodrigues99 <108523049+hmrodrigues99@users.noreply.github.com>
2022-07-01 14:40:42 +0100

    Update fastqdump.sh

--- fastqdump.sh
+++ fastqdump.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-export PATH=$PATH:~/bin/sratoolkit.2.11.1-ubuntu64/bin/ #path to fastq-dump
+'''
+This script runs in loop under the multi_fastqdump.sh.
+This script can be ran alone to retrieve the RNA-seq libraries of a single BioProject accession ID through the SRA (sequence read archive) from NCBI.
+Input:
+1)File named with the BioProject accession ID (ex. PRJNA392919.txt), containing the SRA run codes correspondent to that BioProject, one per line (ex: SRR5986737).
 
-#Input $1 bioproject.txt that contains the SRA sequences.
+#Path to the program fastq-dump
+export PATH=$PATH:~/bin/sratoolkit.2.11.1-ubuntu64/bin/
+
+#Input $1 Bioproject.txt that contains the SRA sequences.
+#Makes new directory, if it doesnt exist already, with the Bioproject access ID.
 dirname=$(echo "$1" | cut -f 1 -d '.')
 if [ ! -d $dirname ]; then
 	mkdir -p $dirname
