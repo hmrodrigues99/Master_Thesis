fba19544f402c4c54b39ca5712fc595e2349d2ed
hmrodrigues99 <108523049+hmrodrigues99@users.noreply.github.com>
2022-08-09 18:54:58 +0100

    Update single_star.sh

--- single_star.sh
+++ single_star.sh
@@ -39,6 +39,6 @@ for i in $(ls -1 aligned_reads/*Aligned.out.bam); do bam_list+=" ${i}"; done
 featureCounts -T 5 -t exon -g gene -a ~/data/genome/GCF_002906115.1_CorkOak1.0_genomic.gff -o ${bioproject_name}raw_counts.txt $bam_list
 
 #This script uses the raw_counts.txt.summary to compute a provisional featureCounts_log.csv
-log_featureCounts.sh ${bioproject_name}raw_counts.txt.summary
+log_featureCounts.sh ${bioproject_name}raw_counts.txt.summary 's'
 #This script merges the trimmomatic_quality.csv, the STAR_log.csv and the featureCounts_log.csv into a BioProject_Statistics.csv
 log_final.sh ${bioproject_name}
