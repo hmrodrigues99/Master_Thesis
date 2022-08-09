1c1fb9ad901b9004ba226954d11f22dda30e0696
hmrodrigues99 <108523049+hmrodrigues99@users.noreply.github.com>
2022-08-09 17:48:45 +0100

    Update log_star.sh

--- log_star.sh
+++ log_star.sh
@@ -4,26 +4,30 @@
 
 echo $1
 
-#number of input reads
+#number of input reads pairs
 input_reads_n=$(grep "Number of input reads" $1 | grep -o '[[:digit:]]*')
-echo $input_reads_n
+#echo $input_reads_n
 
 #uniquely mapped reads number
 uniq_map_reads_n=$(grep "Uniquely mapped reads number" $1 | grep -o '[[:digit:]]*')
-echo $uniq_map_reads_n
+#echo $uniq_map_reads_n
 
-#uniquely mapped reads percentage
+#uniquely mapped read pairs number
+uniq_map_reads_double_n2=$(($uniq_map_reads_n))
+uniq_map_reads_double_n=$(($uniq_map_reads_double_n2*2))
+
+#uniquely mapped reads pairs percentage
 uniq_map_reads_p=$(grep "Uniquely mapped reads %" $1 | grep -o '[[:digit:]][[:digit:]]*.[[:digit:]]*')
-echo $uniq_map_reads_p
+#echo $uniq_map_reads_p
 
 #total multi-mapped/unmapped and chimeric reads
 mm_um_cr_n=$(($input_reads_n-$uniq_map_reads_n))
-echo $mm_um_cr_n
+#echo $mm_um_cr_n
 
 #number of unmapped reads (due to lack of annotation)
 unmapped_reads_short=$(grep "Number of reads unmapped: too short" $1 | grep -o '[[:digit:]]*')
 unmapped_reads_other=$(grep "Number of reads unmapped: other" $1 | grep -o '[[:digit:]]*')
 unmapped_reads_n=$(($unmapped_reads_short+$unmapped_reads_other))
-echo $unmapped_reads_n
+#echo $unmapped_reads_n
 
-printf "${uniq_map_reads_n} \t ${uniq_map_reads_p} \t ${mm_um_cr_n} \t ${unmapped_reads_n} \n" >> STAR_log.csv
+printf "${uniq_map_reads_double_n} \t ${uniq_map_reads_p} \t ${mm_um_cr_n} \t ${unmapped_reads_n} \n" >> STAR_log.csv
