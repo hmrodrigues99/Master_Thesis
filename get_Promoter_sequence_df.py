718c55273428a8176bee657f4a745fb13e7f9bbd
hmrodrigues99 <108523049+hmrodrigues99@users.noreply.github.com>
2022-10-13 16:03:28 +0100

    Update and rename tf_target_promoter_sequence.py to get_Promoter_sequence_df.py

--- get_Promoter_sequence_df.py
+++ get_Promoter_sequence_df.py
@@ -42,28 +42,3 @@ scafold_gene_link = promoter_co[["Scafold", "Gene"]]
 genes = scafold_gene_link["Gene"]
 query_promoter_sequences = pd.merge(nice_promoter_sequences, genes, left_index = True, right_index = True)
 #query_promoter_sequences.to_csv("C:/Users/Hugo Rodrigues/Documents/TF_Targets/Promoters/Promoter_sequences_df.csv", index=False)
-
-#Get the Target gene list of the Query TF
-target_list = []
-filtered_network = network[network["LOC1"].str.contains(TF) | network["LOC2"].str.contains(TF)]
-for LOC in filtered_network["LOC1"]:
-    if not LOC == TF:
-        target_list.append(LOC)
-for LOC in filtered_network["LOC2"]:
-    if not LOC == TF:
-        target_list.append(LOC)
-
-#Get rows corresponding to the target genes of the query TF
-queried_rows = query_promoter_sequences.loc[query_promoter_sequences["Gene"].isin(target_list)]
-'''
-#Get Gene, Scafold and Promoter sequence and start Writing an output fasta file
-fasta_output = open(r'C:/Users/Hugo Rodrigues/Documents/TF_Targets/Promoters/TF_' + TF + '_fasta.fa','w')
-for row_index in range(0, len(queried_rows)):
-    S = queried_rows["Scafold"].values[row_index]
-    G = queried_rows["Gene"].values[row_index]
-    seq = queried_rows["Sequence"].values[row_index]
-    one_entry = ">" + G + " " + S + "\n" + seq + "\n"
-    one_entry = one_entry.upper()
-    fasta_output.write(one_entry)
-fasta_output.close()
-
